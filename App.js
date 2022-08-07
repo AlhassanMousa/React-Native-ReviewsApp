39fff765bf72cc94abb8884f1e3a7f54fda8c810
Alhassan Mousa <AlhassanMousa97@gmail.com>
2022-08-07 11:39:55 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading'
 import { useFonts } from 'expo-font';
 import Navigator from './routes/drawer'
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 
 export default function App() {
   let [fontsLoaded] = useFonts({  
